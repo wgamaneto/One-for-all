da678e1f09d7f5c4652bafa249a0738d963c28ab
washington gama <wgamaneto16@gmail.com>
2022-11-30 17:49:37 -0300

    2

---
+++ desafio2.sql
@@ -0,0 +1,4 @@
+SELECT 
+	(SELECT COUNT(id_musica) FROM SpotifyClone.Musica) AS cancoes, 
+	(SELECT COUNT(id_artista) FROM SpotifyClone.Artista) AS artistas,
+    (SELECT COUNT(id_album) FROM SpotifyClone.Album) AS albuns;
